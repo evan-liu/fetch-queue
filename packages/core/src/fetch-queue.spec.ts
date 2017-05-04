662add38c31a95f74afae4b60ed1e168d45be5e7
Evan Liu <evancoding@gmail.com>
2017-05-05 10:16:37 +1200

    Fetch queue add() and cancel()

--- packages/core/src/fetch-queue.spec.ts
+++ packages/core/src/fetch-queue.spec.ts
@@ -1,13 +1,75 @@
+import * as fetchMock from "fetch-mock";
 import {FetchQueue} from "./fetch-queue";
 
-describe("constructor", () => {
+beforeEach(() => {
+  fetchMock.mock("*", 200);
+});
+afterEach(() => {
+  fetchMock.reset();
+});
 
-  it("read options", () => {
-    expect(new FetchQueue().options.maxConnections).toBe(4);
-    expect(new FetchQueue({maxConnections: 3}).options.maxConnections).toBe(3);
+test("read options", () => {
+  expect(new FetchQueue().options.maxConnections).toBe(4);
+  expect(new FetchQueue({maxConnections: 3}).options.maxConnections).toBe(3);
 
-    expect(new FetchQueue().options.baseURL).toBeUndefined();
-    expect(new FetchQueue({baseURL: "/tests"}).options.baseURL).toBe("/tests");
+  expect(new FetchQueue().options.baseURL).toBeUndefined();
+  expect(new FetchQueue({baseURL: "api"}).options.baseURL).toBe("api");
+});
+
+test("pending for maxConnections", (done) => {
+  expect.assertions(4);
+
+  const queue = new FetchQueue({maxConnections: 2});
+
+  queue.add("test/1");
+  queue.add("test/2");
+  queue.add("test/3");
+  queue.add("test/4").then(() => {
+    expect(queue.activeCount).toBe(0);
+    expect(queue.pendingCount).toBe(0);
+    done();
   });
 
+  expect(queue.activeCount).toBe(2);
+  expect(queue.pendingCount).toBe(2);
+});
+
+test("add base url to path", () => {
+  new FetchQueue().add("/tests");
+  new FetchQueue({baseURL: "api"}).add("/tests");
+
+  expect(fetchMock.calls().matched.map((i) => i[0])).toEqual([
+    "/tests",
+    "api/tests",
+  ]);
+
+});
+
+test("cancel requests", (done) => {
+  const queue = new FetchQueue({maxConnections: 2});
+  expect.assertions(6);
+
+  queue.add("test/1");
+  const p2 = queue.add("test/2");
+  const p3 = queue.add("test/3");
+  const p4 = queue.add("test/4");
+
+  const failMock2 = jest.fn();
+  const failMock4 = jest.fn();
+  p2.catch(failMock2);
+  p4.catch(failMock4);
+
+  p2.cancel();
+  p4.cancel();
+
+  expect(queue.pendingCount).toBe(1); // Remove pending request right away
+  expect(queue.activeCount).toBe(2); // Wait for active connection
+
+  p3.then(() => {
+    expect(failMock2).toHaveBeenCalled();
+    expect(failMock4).toHaveBeenCalled();
+    expect(queue.activeCount).toBe(0);
+    expect(queue.pendingCount).toBe(0);
+    done();
+  });
 });
--- packages/core/src/fetch-queue.ts
+++ packages/core/src/fetch-queue.ts
@@ -1,17 +1,134 @@
+/** Options for the fetch queue and all fetch requests. */
 export interface IFetchQueueOptions {
+
+  /** Max live connection count of the queue. Default to 4. */
   readonly maxConnections?: number;
+
+  /** Base url for server endpoint paths. */
   readonly baseURL?: string;
+
 }
 
+/** Promise that can cancel the request.  */
+export interface IFetchQueuePromise<T = any> extends Promise<T> {
+
+  /** Cancel the request and remove it from the queue. */
+  cancel(): void;
+
+}
+
+enum ItemState {
+  Pending,
+  Active,
+  Succeeded,
+  Failed,
+  Canceled,
+}
+
+interface IQueueItem {
+  readonly url: string;
+  readonly init?: RequestInit;
+  readonly resolve: (value: Response) => void;
+  readonly reject: (reason?: any) => void;
+  state: ItemState;
+}
+
+/**
+ * Request queue base on fetch() API.
+ */
 export class FetchQueue {
 
+  /** Fetch queue options. */
   public readonly options: IFetchQueueOptions;
 
+  /** Count of requests pending in the queue. */
+  public get pendingCount(): number {
+    return this.pendingItems.length;
+  }
+
+  /** Count of active request sending out. */
+  public get activeCount(): number {
+    return this.activeItems.length;
+  }
+
+  private pendingItems: IQueueItem[] = [];
+  private activeItems: IQueueItem[] = [];
+
+  /**
+   * Create a fetch queue.
+   * @param options Queue options.
+   */
   public constructor(options?: IFetchQueueOptions) {
     this.options = {
       maxConnections: 4,
       ...options,
     };
+  }
+
+  /**
+   * Add a request to the end of the queue.
+   *
+   * @param url Request url or path (requires `baseURL` in options)
+   * @param init Request init for fetch() API
+   * @returns {IFetchQueuePromise} Request promise that can also cancel the request.
+   */
+  public add(url: string, init?: RequestInit): IFetchQueuePromise<Response> {
+    if (this.options.baseURL && url[0] === "/") {
+      url = this.options.baseURL + url;
+    }
+
+    let item: IQueueItem;
+    const promise = new Promise((resolve, reject) => {
+      item = {url, init, resolve, reject, state: ItemState.Pending};
+      this.pendingItems.push(item);
+    }) as IFetchQueuePromise;
+    promise.cancel = () => this.cancel(item);
+
+    this.checkNext();
+
+    return promise;
+  }
+
+  private cancel(item: IQueueItem) {
+    switch (item.state) {
+      case ItemState.Pending:
+        this.pendingItems = this.pendingItems.filter((i) => i !== item);
+        break;
+      case ItemState.Active:
+        // It is sending out. Cannot really cancel the request. Just ignore the response.
+        break;
+      default:
+        return;
+    }
+    item.state = ItemState.Canceled;
+    item.reject("Canceled");
+  }
+
+  private checkNext() {
+    while (this.pendingCount > 0 && this.activeCount < this.options.maxConnections!) {
+      const item = this.pendingItems.shift()!;
+      this.activeItems.push(item);
+      item.state = ItemState.Active;
+      fetch(item.url, item.init).then(
+        (response) => this.handleResult(item, ItemState.Succeeded, response),
+        (reason) => this.handleResult(item, ItemState.Failed, reason),
+      );
+    }
+  }
+
+  private handleResult(item: IQueueItem, state: ItemState, result: any) {
+    this.activeItems = this.activeItems.filter((i) => i !== item);
+
+    if (item.state === ItemState.Active) {
+      item.state = state;
+      if (state === ItemState.Succeeded) {
+        item.resolve(result);
+      } else {
+        item.reject(result);
+      }
+    }
+
+    this.checkNext();
   }
 
 }
